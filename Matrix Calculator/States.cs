f79d07e82e0ba340cea1cb306b9a3f02d08500fe
abatek <44042291+abatek@users.noreply.github.com>
2020-01-14 14:27:36 -0500

    Addition of enum

--- Matrix Calculator/States.cs
+++ Matrix Calculator/States.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Matrix_Calculator
 {
-    enum States
+    public enum States
     {
         SELECT,     // select matrix to perform operation (press operation next)
         STORE,      // store currently shown matrix (press storage key next; ie A,B,C,D,E)
